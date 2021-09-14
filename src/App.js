318ee6134a25b517a1e52843a50cbcb9b5785300
kee111 <eqrlgrey@gmail.com>
2021-09-15 02:30:28 +0900

    日時表示

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import React, { useState, useEffect } from "react";
 import Demo from "./components/demo";
+const date = new Date();
 
 function App() {
+    const [dateTime, setDateTime] = useState({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    });
+    useEffect(() => {
+        const timer = setInterval(() => {
+            const date = new Date();
+            setDateTime({
+                hours: date.getHours(),
+                minutes: date.getMinutes(),
+                seconds: date.getSeconds(),
+            });
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
     return (
-        <div　className="container">
-            <Demo />
-        </div>
+        <>
+            <div className="container">
+                <div>
+                    <Demo />
+                </div>
+                <div>
+                    {" "}
+                    {dateTime.hours}:{dateTime.minutes}:{dateTime.seconds}
+                </div>
+            </div>
+        </>
     );
 }
 
